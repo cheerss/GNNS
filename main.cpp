0f76784496304691f8371057f02e885f73f842b5
cheers <zjdxwwx@163.com>
2017-03-19 21:35:07 +0800

    ?

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <utility>
 #include <algorithm>
 #include <string>
+#include "main.h"
 
 using namespace std;
 
@@ -22,23 +23,6 @@ using namespace std;
 #define T 10
 //#define BUILD
 
-template <typename Type>
-void print_vector(vector<vector<Type> > vec);
-template <typename Type>
-vector<vector<Type> > read_data(string, int);
-float distance(vector<float>, vector<float>);
-bool cmp(pair<int, float> a, pair<int, float> b);
-void build_graph();
-void read_graph();
-vector<int> N(int Y, vector<vector <int> > G);
-int argmin_p(int Y, vector<int> N, vector<float> Q, set<pair<int, float> > *S_U);
-vector<int> search_K_nearest(vector<float> Q);// return
-vector<int> search_K_nearest_(vector<float> Q);
-vector<vector<float> > read_Q(int count);
-vector<int> ground_truth(int Q, int count);//which query and return count results
-
-vector<vector<float> > base_vectors; //coordinates of all points
-vector<vector<int> > graph;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
@@ -64,7 +48,6 @@ int main() {
             cout << truth[j] << " ";
         cout << endl;
     }
-
     return 0;
 }
 
--- main.h
+++ main.h
@@ -5,4 +5,25 @@
 #ifndef GNNS_MAIN_H
 #define GNNS_MAIN_H
 
+#include <vector>
+using namespace std;
+
+template <typename Type>
+void print_vector(vector<vector<Type> > vec);
+template <typename Type>
+vector<vector<Type> > read_data(string, int);
+float distance(vector<float>, vector<float>);
+bool cmp(pair<int, float> a, pair<int, float> b);
+void build_graph();
+void read_graph();
+vector<int> N(int Y, vector<vector <int> > G);
+int argmin_p(int Y, vector<int> N, vector<float> Q, set<pair<int, float> > *S_U);
+vector<int> search_K_nearest(vector<float> Q);// return
+vector<int> search_K_nearest_(vector<float> Q);
+vector<vector<float> > read_Q(int count);
+vector<int> ground_truth(int Q, int count);//which query and return count results
+
+vector<vector<float> > base_vectors; //coordinates of all points
+vector<vector<int> > graph;
+
 #endif //GNNS_MAIN_H
